3df958610907a12126941e5f92970cfdd80949c7
Sobhi Malak <sobhi@cloudulu.com>
2020-12-10 16:06:49 +0100

    problems with language

---
+++ Lang/translation.js
@@ -0,0 +1,25 @@
+
+let langdata = {
+  "languages": {
+    "en": {
+      "strings": {
+        "home": "Home",
+        "about": "About",
+        "menu": "Menu",
+        "events": "Events",
+        "contact": "Contact",
+        "booking": "Booking"
+      }
+   },
+   "sv": {
+      "strings": {
+        "home": "Hem",
+        "about": "Om",
+        "menu": "Menu",
+        "events": "Events",
+        "contact": "Contact",
+        "booking": "Boka"
+      }
+    }
+  }
+}
