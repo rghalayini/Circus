13e5348235a78be591c8b095a2754f210a4d4b6f
Daryl <ng.daryl@gmail.com>
2020-12-17 08:46:53 +0100

    improved banner and translations

--- Lang/translation.js
+++ Lang/translation.js
@@ -3,22 +3,52 @@ let langdata = {
   "languages": {
     "en": {
       "strings": {
-        "home": "HOME",
-        "about": "ABOUT",
-        "menu": "MENU",
-        "events": "EVENTS",
-        "contact": "CONTACT",
-        "booking": "BOOK Now"
+        "Home": "Home",
+        "About us": "About us",
+        "Menu": "Menu",
+        "Events": "Events",
+        "Contact us": "Contact us",
+        "Book Now": "BOOK NOw",
+        "welcome": "Welcome to Circus Grill",
+        "circus-grill":"Circus Grill",
+        "slogan": "Vegan Grill and Circus",
+        "orderButton": "ORDER AND PAY IN THE APP",
+        "Meet": "Meet The Chef",
+        "chefSlogan":"Circus Grill Executive Chef",
+        "chef-bio":"Haithem took a liking to cooking already as a young boy. The decision was triggered by a mere fate – his mother found an ad for a restaurant school in the newspaper. Haithem applied, and the rest is history.",
+        "chef-quest":"What does it take to become a good cook?",
+        "chef-answer":"Listen to trends, but don’t necessarily follow them.",
+        "chef-answer1":"You have to taste your own food while cooking.",
+        "chef-quest1":"This is the kitchen that never sleeps. How do you do it?",
+        "chef-answer3":"That’s true – we are open all day and all night.",
+        "chef-answer4":"Every person in this kitchen has their own unique position.",
+        
       }
    },
    "sv": {
       "strings": {
-        "home": "HEM",
-        "about": "OM OSS",
-        "menu": "MENY",
-        "events": "EVENT",
-        "contact": "KOTAKTA OSS",
-        "booking": "BOKA NU"
+        "Home": "Hem",
+        "About us": "Om oss",
+        "Menu": "Meny",
+        "Events": "Event",
+        "Contact us": "Kontakta oss",
+        "Book Now": "BOKA NU",
+        "welcome": "Välkommen till Circus Grill",
+        "circus-grill":"Circus Grill",
+        "slogan": "Vegansk grill och cirkus",
+        "orderButton": "BESTÄLL OCH BETALA I APPEN",
+        "Meet": "vår kökschef",
+        "chefSlogan":"Circus Grill Kökschef",
+        "chef-bio":"Haithem tyckte om att laga mat redan som ung pojke. Beslutet utlöstes av enbart öde - hans mamma hittade en annons för en restaurangskola i tidningen. Haithem tillämpade, och resten är historia.",
+        "chef-quest":"Vad krävs för att bli en bra kock?",
+        "chef-answer":"Lyssna på trender, men följ inte nödvändigtvis dem.",
+        "chef-answer1":"Du måste smaka på din egen mat medan du lagar mat.",
+        "chef-quest1":"Det här är köket som aldrig sover. Hur gör du det?",
+        "chef-answer3":"Det är sant - vi är öppna hela dagen och hela natten.",
+        "chef-answer4":"Varje person i detta kök har sin egen unika position.",
+
+
+
       }
     }
   }
